02127426f75dbe43dae9d90f932929dc04def53c
Justin Page <justin.page89@gmail.com>
2020-08-14 12:14:05 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 count = 10
 while count >= 1 do
 puts count  
   count -= 1
 end
 puts "Happy New Year!"
+=======
+# Write your code here
+while -1 do 
+  puts "say this forever..."
+end
+>>>>>>> 7af045ba51b0af6ab6a53052515b8dbe30ce7c9c
